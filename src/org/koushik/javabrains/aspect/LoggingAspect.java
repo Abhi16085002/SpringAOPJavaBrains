9256f57d9556fe8a02d7857ce61e99f83fe7b398
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-05-13 19:58:40 +0530

    Spring Tutorial 29 - A Few More Pointcut Expressions

--- src/org/koushik/javabrains/aspect/LoggingAspect.java
+++ src/org/koushik/javabrains/aspect/LoggingAspect.java
@@ -7,7 +7,7 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect
 public class LoggingAspect {
 
-	@Before("allGetters()")
+	@Before("allGetters() || allCircleMethods()")  // use && for and operation
 	public void loggingAdvice() {
 		System.out.println("Advice run. Get method Called");
 	}
@@ -19,4 +19,9 @@ public class LoggingAspect {
 	
 	@Pointcut("execution(* get*())")  // .. match zero or more character. one can use package for executing method.
 	public void allGetters () {} 
+	
+	@Pointcut("within(org.koushik.javabrains.model.Circle)")   // execution(* * org.koushik.javabrains.model.circle.*)
+	public void allCircleMethods() {}
+	
+//	@Pointcut(args(..*)) an example check spring site
 }
