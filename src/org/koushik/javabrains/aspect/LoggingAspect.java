2496ebc28eabdebf0fecb75a011f37fab0845d00
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-05-13 18:10:20 +0530

    Spring Tutorial 28 - Pointcuts and Wildcard Expressions

--- src/org/koushik/javabrains/aspect/LoggingAspect.java
+++ src/org/koushik/javabrains/aspect/LoggingAspect.java
@@ -2,12 +2,21 @@ package org.koushik.javabrains.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
-	@Before("execution(public String getName())")
+	@Before("allGetters()")
 	public void loggingAdvice() {
 		System.out.println("Advice run. Get method Called");
 	}
+	
+	@Before("allGetters()")
+	public void secondAdvice() {
+		System.out.println("Second Advice executed.");
+	}
+	
+	@Pointcut("execution(* get*())")  // .. match zero or more character. one can use package for executing method.
+	public void allGetters () {} 
 }
