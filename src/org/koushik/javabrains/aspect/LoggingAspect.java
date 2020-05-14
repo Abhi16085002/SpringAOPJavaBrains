6869a61e0fec456b081291207c680ab95545f5fa
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-05-14 16:35:11 +0530

    Spring Tutorial 34 - AOP XML configuration

--- src/org/koushik/javabrains/aspect/LoggingAspect.java
+++ src/org/koushik/javabrains/aspect/LoggingAspect.java
@@ -11,29 +11,9 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.koushik.javabrains.model.Circle;
 
-@Aspect
 public class LoggingAspect {
 
-//	@Before("allCircleMethods()")  
-//	public void loggingAdvice(JoinPoint joinPoint) {
-//		System.out.println(joinPoint.toString());
-//		System.out.println(joinPoint.getTarget());
-//		Circle circle = (Circle) joinPoint.getTarget();
-//		System.out.println(circle.getName() + " doooooooooo");
-//		
-//	}
-	
-	@AfterReturning(pointcut="args(name)",returning="returnString")
-	public void stringArgumentMethod(String name,String returnString ) {
-		System.out.println("A method that takes string argument has been called. The value is " + name + ". The output value is " + returnString );
-	}
-	
-	@AfterThrowing(pointcut="args(name)", throwing="ex" )
-	public void exceptionAdvice(String name,Exception ex) {
-		System.out.println("An exception has been thrown "+ ex);
-	}
-	
-	@Around("@annotation(org.koushik.javabrains.aspect.Loggable)")
+	@Around("allGetters()")
 	public Object myAroundAdvice(ProceedingJoinPoint proceedingJoinPoint ) {
 		
 		Object returnValue = null;
@@ -52,10 +32,7 @@ public class LoggingAspect {
 		return returnValue;
 	}
 	
-	@Pointcut("execution(* get*())") 
-	public void allGetters () {} 
-	
-	@Pointcut("within(org.koushik.javabrains.model.Circle)")   
-	public void allCircleMethods() {}
+//	@Pointcut("execution(* get*())") 
+//	public void allGetters () {} 
 	
 }
